4d44a2fb7cb4613aba2ebc15ad581116bffd943c
xd Abitur <abinayansureshkumar@gmail.com>
2021-04-17 09:55:59 +0200

    Basic project setup using premake5

---
+++ Minigames/premake5.lua
@@ -0,0 +1,32 @@
+project "Minigames"
+    kind "ConsoleApp"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "src"
+    }
+    
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+    filter "configurations:Dist"
+        optimize "On"
---
+++ premake5.lua
@@ -0,0 +1,13 @@
+workspace "Minigames"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "Minigames"
